f9f865cdbd3f5bd993b2db58e754eac76211c462
hassangholipoor <hassangpoor@gmail.com>
2022-05-17 17:31:59 +0430

    Fixed authorAccessPermission

--- app/article/permissions.py
+++ app/article/permissions.py
@@ -1,10 +1,15 @@
 from rest_framework import permissions
+from core.models import Article
 
 
 class AuthorAccessPermission(permissions.BasePermission):
 
     def has_permission(self, request, view):
-        if request.user.is_anonymous == True:
+        if request.user.is_anonymous:
             return False
         elif request.user.is_author:
+            return True
+
+    def has_object_permission(self, request, view, obj):
+        if obj.owner == request.user or request.user.is_superuser:
             return True
